d80dce34dd168db0b69fe3426b7350c34a82566b
gmatosbatista <leozancintra@gmail.com>
2023-04-08 10:03:30 -0300

    Atualização main_info_update

--- assets/js/main_info_update.js
+++ assets/js/main_info_update.js
@@ -4,18 +4,15 @@ function update_main_info_toshl() {
   // Selecionar o elemento que será atualizado
   const total_all_accountsElement = document.getElementById('total_all_accounts');
 
-  // Executar a consulta SQL para somar a coluna "balance" na tabela "toshl_accounts"
-  fetch('https://gestaopatrimonio.herokuapp.com/api/sql', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ query: 'SELECT SUM(balance) AS total_all_accounts FROM toshl_accounts' })
+// Executar a consulta ao servidor para buscar o total Balance do Toshl"
+fetch('https://gestaopatrimonio.herokuapp.com/api/total_all_accounts')
+  .then(response => response.json())
+  .then(data => {
+    const totalBalanceElement = document.getElementById('total_balance');
+    totalBalanceElement.textContent = data.total_all_accounts;
   })
-    .then(response => response.json())
-    .then(data => {
-      const total_all_accounts = data[0].total_all_accounts;
-      total_all_accountsElement.textContent = total_all_accounts.toFixed(2);
-    })
-    .catch(error => console.error(error));
+  .catch(error => console.error(error));
+
 }
 
 window.addEventListener('load', update_main_info_toshl);
