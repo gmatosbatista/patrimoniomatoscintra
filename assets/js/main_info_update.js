a485b5284f375171d64d0345e0c6ccbbe01f158f
gmatosbatista <leozancintra@gmail.com>
2023-04-12 08:10:15 -0300

    Atualização main_info_update

--- assets/js/main_info_update.js
+++ assets/js/main_info_update.js
@@ -4,15 +4,17 @@ function update_main_info_toshl() {
   // Selecionar o elemento que será atualizado
   const total_all_accountsElement = document.getElementById('total_all_accounts');
 
-// Executar a consulta ao servidor para buscar o total Balance do Toshl"
-fetch('https://gestaopatrimonio.herokuapp.com/api/total_all_accounts')
-  .then(response => response.json())
-  .then(data => {
-    const totalBalanceElement = document.getElementById('total_balance');
-    totalBalanceElement.textContent = data.total_all_accounts;
-  })
-  .catch(error => console.error(error));
+  // Executar a consulta ao servidor para buscar o total Balance do Toshl"
+  fetch('https://gestaopatrimonio.herokuapp.com/api/total_all_accounts')
+    .then(response => response.json())
+    .then(data => {
+      // Arredondar o valor para 2 casas decimais
+      const totalBalance = parseFloat(data.total_all_accounts).toFixed(2);
 
+      // Atualizar o elemento HTML com o valor arredondado
+      total_all_accountsElement.textContent = totalBalance;
+    })
+    .catch(error => console.error(error));
 }
 
 window.addEventListener('load', update_main_info_toshl);
