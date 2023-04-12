626e3692f7bd51a94ce22067d45fa49b02e77b71
gmatosbatista <leozancintra@gmail.com>
2023-04-12 09:16:37 -0300

    Segundo commit

--- assets/js/main_info_update.js
+++ assets/js/main_info_update.js
@@ -1,20 +1,20 @@
-function update_main_info_toshl() {
-  console.log('update_main_info_toshl() called');
+function patrimonio_total() {
+  console.log('patrimonio_total() called');
   
   // Selecionar o elemento que será atualizado
-  const total_all_accountsElement = document.getElementById('total_all_accounts');
+  const patrimonio_totalElement = document.getElementById('patrimonio_total');
 
   // Executar a consulta ao servidor para buscar o total Balance do Toshl"
-  fetch('https://gestaopatrimonio.herokuapp.com/api/total_all_accounts')
+  fetch('https://gestaopatrimonio.herokuapp.com/api/patrimonio_total')
     .then(response => response.json())
     .then(data => {
       // Arredondar o valor para 2 casas decimais
-      const totalBalance = parseFloat(data.total_all_accounts).toFixed(2);
+      const patrimonio_total = parseFloat(data.patrimonio_total).toFixed(0);
 
       // Atualizar o elemento HTML com o valor arredondado
-      total_all_accountsElement.textContent = totalBalance;
+      patrimonio_totalElement.textContent = patrimonio_total;
     })
     .catch(error => console.error(error));
 }
 
-window.addEventListener('load', update_main_info_toshl);
+window.addEventListener('load', patrimonio_total);
